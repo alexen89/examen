c2f8b34dd3b0e24662becee4ca76d48d809e10a6
Alvaro Alejandro Enciso juarez <aalvaro.encisoj@outllok.com>
2017-04-02 17:23:08 -0500

    se agrega configuracion de bd

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'examen';
+$db['default']['password'] = 'examen';
+$db['default']['database'] = 'examen';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
